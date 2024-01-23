0762b0a03fd457fd409c8d4a9702ab1168f5c2b1
Yunzhao Li <yunzhao.li@mail.utoronto.ca>
2024-01-23 05:38:36 +0000

    finish test script

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,20 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Tests the class and value of time_period and death_licenses.
+# Author: Yunzhao Li
+# Date: 22 January 2024
+# Contact: yunzhao.li@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: 02-data_cleaning.R
 
 
 #### Workspace setup ####
+library(janitor)
+library(lubridate)
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Test data ####
+cleaned_death_registry$time_period |>
+  unique() == c(sprintf("2023-0%s-01", c(1:9)), "2023-10-01", "2023-11-01", "2023-12-01")
+cleaned_death_registry$time_period |> class() == "Date"
+cleaned_death_registry$death_licenses |> class() == "numeric"
+
